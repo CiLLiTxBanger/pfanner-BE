bc36cc7e31b8eedae08be37348cf476984d0639a
Florian Blauensteiner <florian.blauen@gmail.com>
2022-05-02 08:40:57 +0200

    Added comments in Tree.py

--- PfannerObstgartenAPI/api/models/Tree.py
+++ PfannerObstgartenAPI/api/models/Tree.py
@@ -19,8 +19,8 @@ class Tree(models.Model):
     cut = models.CharField(max_length=50, blank=False)
     active = models.BooleanField(default=True)
     owner = models.ForeignKey('auth.User', related_name='trees', on_delete=models.CASCADE, default=1)
-    created_on = models.DateTimeField(auto_now_add=True)
-    edited_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)  #auto_now_add -> created time
+    edited_on = models.DateTimeField(auto_now=True)   #auto_now -> changes at each update
     
     class Meta:
         ordering = ('row', 'column', )
