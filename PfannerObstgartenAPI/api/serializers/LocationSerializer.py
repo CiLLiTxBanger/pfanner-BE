4b11da8d43580470ddee9ee9cf7dd708b096b4f9
Florian Blauensteiner <florian.blauen@gmail.com>
2022-05-12 20:33:59 +0200

    added the id field to the location serializer

--- PfannerObstgartenAPI/api/serializers/LocationSerializer.py
+++ PfannerObstgartenAPI/api/serializers/LocationSerializer.py
@@ -4,4 +4,4 @@ from api.models import Location
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
-        fields = ['city', 'country', 'description', 'created_on', 'edited_on'] 
+        fields = ['id', 'city', 'country', 'description', 'created_on', 'edited_on'] 
