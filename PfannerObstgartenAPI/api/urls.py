2a7a1c13e19fc433724beff9a6b7020e59d80dcb
Florian Blauensteiner <florian.blauen@gmail.com>
2022-05-12 13:44:24 +0200

    removed auth requirement (comment)

--- PfannerObstgartenAPI/api/urls.py
+++ PfannerObstgartenAPI/api/urls.py
@@ -4,11 +4,12 @@ from api import views
 
 urlpatterns = [
     path('', views.api_root),
-#     path('api/trees/', views.TreeList.as_view(), name='tree-list'),
-#     path('api/trees/<int:pk>/', views.TreeDetail.as_view(), name='tree-detail'),
-#     path('users/', views.UserList.as_view(), name='user-list'),
-#     path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
-    path('test', views.test.current_datetime)
+     path('api/trees/', views.views.TreeList.as_view(), name='tree-list'),
+     path('api/trees/<int:pk>/', views.views.TreeDetail.as_view(), name='tree-detail'),
+     path('users/', views.views.UserList.as_view(), name='user-list'),
+     path('users/<int:pk>/', views.views.UserDetail.as_view(), name='user-detail'),
+     path('testpostview/', views.testPostView.TestPostView.as_view(), name='testexampleview'),
+#    path('test', views.test.current_datetime)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
