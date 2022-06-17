d1a20ed4ba5ffb2ce8cd6b68fe00a8dcc8b7f371
Johannes Eder <jojo.eder@gmail.com>
2022-06-17 17:23:30 +0200

    Generate CSV LabMeasurement by TreeID

--- PfannerObstgartenAPI/api/views/CsvView.py
+++ PfannerObstgartenAPI/api/views/CsvView.py
@@ -2,7 +2,7 @@
 from rest_framework import permissions
 from rest_framework import generics
 from rest_framework import permissions
-from api.models import OrchardMeasurement, Tree
+from api.models import LabMeasurement, OrchardMeasurement, Tree
 from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 from rest_framework.response import Response
@@ -10,8 +10,24 @@ from rest_framework.views import APIView
 import csv
 
 
-# class ExportLabMeasurementsCSVByTreeId(APIView):
+class ExportLabMeasurementsCSVByTreeId(APIView):
+    def get(self, request, treeId, *args, **kwargs):
+        treeId = treeId
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="LabMeasurements_Tree_{}.csv"'.format(str(treeId))
 
+        writer = csv.writer(response)
+        labMeasurements = LabMeasurement.objects.filter(tree = treeId)
+
+        #Headlines
+        writer.writerow(['sep=,'])
+        writer.writerow(['Strength Measurement', 'Flavor Measurement', 'Acid Measurement', 'Sugar Measurement', 'Status', 'Last Modified', 'Created on'])
+
+        #Rows
+        for lM in labMeasurements:
+            writer.writerow([' '+str(lM.strengthMeasurement), lM.flavorMeasurement, ' '+str(lM.acidMeasurement), ' '+str(lM.sugarMeasurement), lM.status, lM.timestamp.strftime("%d.%m.%Y - %H:%M"), lM.created_on.strftime("%d.%m.%Y - %H:%M")])
+
+        return response
 
 class ExportOrchardMeasurementsCSVByTreeId(APIView):
     def get(self, request, treeId, *args, **kwargs):
@@ -31,6 +47,3 @@ class ExportOrchardMeasurementsCSVByTreeId(APIView):
             writer.writerow([oM.frostSensitivity, oM.growthHabit, oM.yieldHabit, oM.season, oM.temperature, oM.precipitation, oM.lateFrost, oM.status, oM.created_on.strftime("%d.%m.%Y - %H:%M")])
 
         return response
-#         return Response({"success": treeId})
-
-
