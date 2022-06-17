198a25dc98862fc5109b5d9ebb04a0bc87af4ea3
Florian Blauensteiner <florian.blauen@gmail.com>
2022-06-17 23:13:06 +0200

    implemented /api/trees/filterbyorchardmeasurements

--- PfannerObstgartenAPI/api/urls.py
+++ PfannerObstgartenAPI/api/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
      path('api/trees/<int:treeId>/orchardmeasurements/csv', views.CsvView.ExportOrchardMeasurementsCSVByTreeId.as_view(), name='orchardmeasurement-csv'),
      path('api/trees/filterbylabmeasurementstats/', views.LabMeasurementView.TreesFilteredByLabMeasurementStats.as_view(), name='treesfilteredbylabmeasurementstats-list'),
      path('api/trees/<int:pk>/analytics', views.TreeView.TreeAnalytics.as_view(), name='tree-analytics'),
+     path('api/trees/filterbyorchardmeasurementstats/', views.OrchardMeasurementView.TreesFilteredByOrchardMeasurementStats.as_view(), name='treesfilteredbyorchardmeasurementstats-list'),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- PfannerObstgartenAPI/api/views/OrchardMeasurementView.py
+++ PfannerObstgartenAPI/api/views/OrchardMeasurementView.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
-from api.serializers import OrchardMeasurementSerializer, WriteOrchardMeasurementSerializer
-from api.models import OrchardMeasurement
+from api.serializers import OrchardMeasurementSerializer, WriteOrchardMeasurementSerializer, TreeSerializer
+from api.models import OrchardMeasurement, Tree
 from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
@@ -34,3 +34,57 @@ class OrchardMeasurementDetail(generics.RetrieveUpdateDestroyAPIView):
         if self.request.method == 'PUT':
             return WriteOrchardMeasurementSerializer
         return OrchardMeasurementSerializer
+
+class TreesFilteredByOrchardMeasurementStats(generics.ListAPIView):
+    queryset = OrchardMeasurement.objects.all()
+    serializer_class = TreeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = OrchardMeasurement.objects.all()
+        frostSensitivity = self.request.query_params.get('frostSensitivity')
+        growthHabit = self.request.query_params.get('growthHabit')
+        yieldHabit = self.request.query_params.get('yieldHabit')
+        precipitation = self.request.query_params.get('precipitation')
+        temperature = self.request.query_params.get('temperature')
+        season = self.request.query_params.get('season')
+        lateFrost = self.request.query_params.get('lateFrost')
+        filteredIds = []
+        filtersApplied = False
+
+        if frostSensitivity is not None:
+            filtersApplied = True
+            queryset = queryset.filter(frostSensitivity=frostSensitivity)
+
+        if growthHabit is not None:
+            filtersApplied = True
+            queryset = queryset.filter(growthHabit=growthHabit)
+
+        if yieldHabit is not None:
+            filtersApplied = True
+            queryset = queryset.filter(yieldHabit=yieldHabit)
+
+        if precipitation is not None:
+            filtersApplied = True
+            queryset = queryset.filter(precipitation=precipitation)
+
+        if temperature is not None:
+            filtersApplied = True
+            queryset = queryset.filter(temperature=temperature)
+
+        if season is not None:
+            filtersApplied = True
+            queryset = queryset.filter(season=season)
+
+        if lateFrost is not None:
+            filtersApplied = True
+            queryset = queryset.filter(lateFrost=lateFrost)
+
+        if filtersApplied:
+            filteredIds.append(queryset.values_list('tree_id', flat=True))
+            queryset = Tree.objects.all()
+            queryset = queryset.filter(pk__in=filteredIds)
+        else:
+            queryset = Tree.objects.all()
+
+        return queryset
