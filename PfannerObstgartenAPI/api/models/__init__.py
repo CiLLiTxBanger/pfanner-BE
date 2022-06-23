25cae6d1918e0cf2f7a63fac0b58c4a3d29e4e24
Florian Blauensteiner <florian.blauen@gmail.com>
2022-06-23 20:47:11 +0200

    adjusted init.py of models and serializers to not import removed files

--- PfannerObstgartenAPI/api/models/__init__.py
+++ PfannerObstgartenAPI/api/models/__init__.py
@@ -5,18 +5,5 @@ from .Location import Location
 from .Variety import Variety
 from .Disease import Disease
 from .DiseaseMeasurement import DiseaseMeasurement
-from .Image import Image
 from .LabMeasurement import LabMeasurement
 from .OrchardMeasurement import OrchardMeasurement
-
-#Obsolete bc values are now directly in lab- orchardMeasurement
-#from .AcidMeasurement import AcidMeasurement
-#from .FlavorMeasurement import FlavorMeasurement
-#from .FrostSensitivity import FrostSensitivity
-#from .GrowthHabit import GrowthHabit
-#from .Precipitation import Precipitation
-#from .Season import Season
-#from .StrengthMeasurement import StrengthMeasurement
-#from .SugarMeasurement import SugarMeasurement
-#from .Temperature import Temperature
-#from .YieldHabit import YieldHabit
--- PfannerObstgartenAPI/api/serializers/__init__.py
+++ PfannerObstgartenAPI/api/serializers/__init__.py
@@ -1,21 +1,10 @@
 import imp
 from .TreeSerializer import TreeSerializer, WriteTreeSerializer
 from .LocationSerializer import LocationSerializer
-from .ImageSerializer import ImageSerializer
 from .VarietySerializer import VarietySerializer
-from .AcidMeasurementSerializer import AcidMeasurementSerializer
 from .DiseaseSerializer import DiseaseSerializer
 from .DiseaseMeasurementSerializer import DiseaseMeasurementSerializer, WriteDiseaseMeasurementSerializer
-from .FlavorMeasurementSerializer import FlavorMeasurementSerializer
-from .FrostSensivitySerializer import FrostSensitivitySerializer
-from .GrowthHabitSerializer import GrowthHabitSerializer
 from .LabMeasurementSerializer import LabMeasurementSerializer, WriteLabMeasurementSerializer
 from .OrchardMeasurementSerializer import OrchardMeasurementSerializer, WriteOrchardMeasurementSerializer
-from .PrecipitationSerializer import PrecipitationSerializer
-from .SeasonSerializer import SeasonSerializer
-from .StrengthMeasurementSerializer import StrengthMeasurementSerializer
-from .SugarMeasurementSerializer import SugarMeasurementSerializer
-from .TemperatureSerializer import TemperatureSerializer
-from .YieldHabitSerializer import YieldHabitSerializer
 from .UserSerializer import UserSerializer
 from .TreeAnalyticsSerializer import TreeAnalyticsSerializer
