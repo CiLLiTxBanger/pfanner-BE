3ccb1df3817b2b6dafff2176f77edfd631b530da
Florian Blauensteiner <florian.blauen@gmail.com>
2022-06-24 13:42:21 +0200

    adjusted fields of variety and tree to be optional

--- PfannerObstgartenAPI/api/models/Tree.py
+++ PfannerObstgartenAPI/api/models/Tree.py
@@ -2,7 +2,7 @@ from django.db import models
 from datetime import datetime
 
 class Tree(models.Model):
-    type = models.CharField(max_length=50, blank=False)
+    type = models.CharField(max_length=50, blank=True, null=True)
     variety = models.ForeignKey(
         'Variety',
         on_delete=models.SET(999999)
@@ -14,10 +14,10 @@ class Tree(models.Model):
         'Location',
         on_delete=models.CASCADE
     )
-    organic = models.BooleanField()
+    organic = models.BooleanField(blank=True, null=True)
     latitude = models.DecimalField(max_digits=15, decimal_places=10, blank=True, null=True)
     longitude = models.DecimalField(max_digits=15, decimal_places=10, blank=True, null=True)
-    cut = models.CharField(max_length=50)
+    cut = models.CharField(max_length=50, blank=True, null=True)
     active = models.BooleanField(default=True)
     created_on = models.DateTimeField(auto_now_add=True)  #auto_now_add -> created time
     edited_on = models.DateTimeField(auto_now=True)   #auto_now -> changes at each update
