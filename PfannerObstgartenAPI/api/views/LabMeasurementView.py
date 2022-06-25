b93b5f0c80dac0c558c9d73fee4dec301455a316
Florian Blauensteiner <florian.blauen@gmail.com>
2022-06-25 13:19:11 +0200

    refactoring / code cleanup

--- PfannerObstgartenAPI/api/views/LabMeasurementView.py
+++ PfannerObstgartenAPI/api/views/LabMeasurementView.py
@@ -15,18 +15,7 @@ class LabMeasurementListByTreeId(APIView):
     def get(request, self, treeId, format=None):
         labMeasurements = LabMeasurement.objects.filter(tree = treeId)
         serializer = LabMeasurementSerializer(labMeasurements, many = True)
-        permission_classes = [permissions.IsAuthenticated]
-        #permission_classes = [admin]
         return Response(serializer.data)
-
-    def post(request, self, treeId):
-        labMeasurement = self.data
-        #Create LabMeasurement from above data
-        serializer = LabMeasurementSerializer(data=labMeasurement)
-        permission_classes = [permissions.IsAuthenticated]
-        if serializer.is_valid(raise_exception=True):
-            labMeasurement_saved = serializer.save()
-        return Response({"success": "Success!"})
         
 class LabMeasurementList(generics.ListCreateAPIView):
     """
